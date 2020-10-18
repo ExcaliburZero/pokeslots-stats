ca018fb416da44bcd4ffcaff9a9d198f1dbe16d8
Christopher Wells <cwellsny@nycap.rr.com>
2020-10-17 23:52:53 -0700

    Add script that parses csv and prints summary

---
+++ pokeslots-stats/__main__.py
@@ -0,0 +1,35 @@
+from typing import List
+
+import argparse
+import sys
+
+import pandas as pd
+
+
+def main(argv: List[str]) -> None:
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest="command")
+
+    parser_pokemon_info = subparsers.add_parser("pokemon_info", help="lists information on the given pokemon data csv")
+    parser_pokemon_info.add_argument("pokemon_csv")
+
+    args = parser.parse_args(argv)
+
+    if args.command == "pokemon_info":
+        data = pd.read_csv(args.pokemon_csv)
+
+        print(data.groupby(["rarity"]).describe())
+        print(data)
+    elif args.command == None:
+        parser.print_help()
+
+        sys.exit(1)
+    else:
+        print("Invalid command:", args.command)
+        parser.print_help()
+
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
